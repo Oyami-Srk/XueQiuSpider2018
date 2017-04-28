b387de85d6686e1dbd1b02160909cd8cb8f84332
Oyami <hhx.xxm@gmail.com>
2017-04-28 14:22:31 +0800

    Add GetStock

--- Stock.py
+++ Stock.py
@@ -3,6 +3,19 @@
 #Date: 2017-04-27
 
 from FetchPortfolio import request, GetHeader
+import re
+
+def GetStock(Symbol):
+    postUrl = 'https://xueqiu.com/P/'
+    Header = GetHeader()
+    try:
+        resp, cont = request(postUrl + Symbol, header=Header)
+    except:
+        raise Exception('无法获取组合信息！')
+
+    c = re.compile('<span class="stock-name"><div class="name">(.+?)</div><div class="price">(.+?)</div></span>')
+    data = c.findall(cont.decode('utf-8'))
+    return data
 
 if __name__ == '__main__':
     print('For Test')
