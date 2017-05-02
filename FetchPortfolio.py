49ae61aac5db419827a93ea3a4144b3ddd2ded20
ts <vincentbao@gmail.com>
2017-05-02 17:57:49 +0800

    minor change

--- FetchPortfolio.py
+++ FetchPortfolio.py
@@ -91,9 +91,7 @@ def GetRateChartAsDataframe(Symbol, noPercent = True):
 
 def GetRateChartsAsDataframe(Symbols, noPercent = True, ErrorSymbol = []):
     Charts = []
-    n = len(Symbols)
     for Symbol in Symbols:
-        print('%d/%d - %.2f' % (Symbols.index(Symbol), n, (Symbols.index(Symbol) / n) * 100) + '%')
         try:
             Charts.append(GetRateChartAsDataframe(Symbol, noPercent))
         except KeyboardInterrupt:
@@ -110,7 +108,9 @@ def SaveRateChartToHDF5(Symbol, Path = 'RateChart.h5', noPercent = True):
     dataframe.to_hdf(Path, Symbol)
 
 def SaveRateChartsToHDF5(Symbols = [], Path = 'RateChart.h5', noPercent = True, ErrorSymbol = []):
+    n = len(Symbols)
     for Symbol in Symbols:
+        print('%d/%d - %.2f' % (Symbols.index(Symbol), n, (Symbols.index(Symbol) / n) * 100) + '%')
         try:
             SaveRateChartToHDF5(Symbol, Path, noPercent)
             print(' [Done]')
