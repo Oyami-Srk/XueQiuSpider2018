f77e6fe528713f5b3eeb667fb2016d774fcdd975
Oyami <hhx.xxm@gmail.com>
2017-05-02 22:15:51 +0800

    可以用了

--- GetPortfolioFromPages.py
+++ GetPortfolioFromPages.py
@@ -3,7 +3,7 @@
 #Date: 2017-04-27
 
 from FetchPortfolio import request, GetHeader
-import re
+import re, json
 
 def GetPortfolioFromPage(url):
     Header = GetHeader()
@@ -12,7 +12,7 @@ def GetPortfolioFromPage(url):
     except KeyboardInterrupt:
         raise KeyboardInterrupt()
     except:
-        raise Exception('无法获取组合信息！')
+        raise Exception('无法获取页面信息！')
 
     c = re.compile('(<!-- 文章标题-->.*<!-- pdf-->)')
     page = c.findall(cont.decode('utf-8'))[0]
@@ -24,7 +24,47 @@ def GetPortfolioFromPage(url):
             List.append(symbol)
     return List
 
+# 返回一个储存用户的所有帖子中所有匹配regx的地址, 可直接用于GetPortfolioFromPage
+def GetPagesUrl(regx = ''):
+    base = 'https://xueqiu.com/v4/statuses/user_timeline.json?user_id=5171159182&page='
+    if regx == '':
+        regx = '最受关注 & 优质组合调仓路线'
+
+    c = re.compile(regx)
+
+    Header = GetHeader()
+    try:
+        resp, cont = request(base + '0', header = Header)
+    except KeyboardInterrupt:
+        raise KeyboardInterrupt()
+    except:
+        raise Exception('无法获取用户帖子信息！')
+
+    Data = json.loads(cont)
+    max = Data['maxPage']
+    List = []
+
+    for i in range(0, max + 1):
+        try:
+            resp, cont = request(base + str(i), header=Header)
+        except KeyboardInterrupt:
+            print('\nUser Interrupt!\n')
+            break
+        except:
+            raise Exception('无法获取用户帖子信息！')
+
+        try:
+            Data = json.loads(cont)
+            for post in Data['statuses']:
+                if len(c.findall(post['title'])) != 0:
+                    print(post['title'] + ':' + post['target'])
+                    List.append('https://xueqiu.com' + post['target'])
+        except KeyboardInterrupt:
+            print('\nUser Interrupt!\n')
+            break
+
+    return List
+
 
 if __name__ == '__main__':
     print('For Test')
-    
