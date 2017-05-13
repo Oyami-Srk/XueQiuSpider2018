827d5118fbc502cf021102d000cab05ae898d545
Oyami <hhx.xxm@gmail.com>
2017-05-13 20:56:08 +0800

    塔诺西

--- Monitor.py
+++ Monitor.py
@@ -4,10 +4,13 @@
 from PortfolioInfo import *
 import re, json, time, os
 import numpy as np
+import gc
 
 MonitorData = [
     {'Symbol': '', 'Time': 0}
 ]
+
+MonitorDelay = 60 * 60 * 1
 
 def GetLastestTime(Symbol):
     data = GetPortfolioHistories(Symbol)
@@ -16,7 +19,7 @@ def GetLastestTime(Symbol):
     return Time
 
 # 当检测到调仓变化后调用该函数
-def InvokeWhenUpdate(Symbol, Last):
+def InvokeWhenUpdated(Symbol, Last):
     print('不可思议! 标志为%s的组合居然调仓啦~' % Symbol)
     data = GetPortfolioHistories(Symbol)
     i = 0
@@ -25,9 +28,14 @@ def InvokeWhenUpdate(Symbol, Last):
             break
         i = i + 1
     ShowHistories(data[:i])
-    del data
     # You can add your logic here! data[:i] means delta History!
+    # Saving data to history file is best way!
+    del data
     return 'Amazing!'
+
+def InvokeWhenAdded(Symbol):
+    print('组合 %s 进入监视!' % Symbol)
+    # You can add your logic here!
 
 def LoadFromDisk(Filename):
     if os.path.exists(Filename) == False:
@@ -52,8 +60,38 @@ def SaveToDisk(Filename):
     finally:
         fp.close()
 
+def CheckUpdate(Data):
+    for item in Data:
+        if item['Symbol'] == '':
+            continue
+        Last = GetLastestTime(item['Symbol'])
+        if Last != item['Time']:
+            if item['Time'] == 0:
+                InvokeWhenAdded(item['Symbol'])
+            else:
+                InvokeWhenUpdated(item['Symbol'], item['Time'])
+            item['Time'] = Last
+        gc.collect()
 
+def Monitor():
+    global MonitorData
+    MonitorData = LoadFromDisk('Monitor.json')
+    try:
+        while(True):
+            try:
+                CheckUpdate(MonitorData)
+                time.sleep(MonitorDelay)
+            except KeyboardInterrupt:
+                raise KeyboardInterrupt()
+            except Exception as e:
+                print("出现错误: " + e)
+            except:
+                print("大概不可能会出现的其他错误!!!!")
 
+    except KeyboardInterrupt:
+        print("监视终止!")
+    finally:
+        SaveToDisk('Monitor.json')
 
 if __name__ == '__main__':
-    GetLastestTime('ZH888882')
+    Monitor()
