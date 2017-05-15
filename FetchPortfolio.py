c7cc45e89b38ca857d46cef54fdf200ba3640653
ts <vincentbao@gmail.com>
2017-05-15 16:43:37 +0800

    强迫症

--- FetchPortfolio.py
+++ FetchPortfolio.py
@@ -5,17 +5,18 @@
 #Follow the installation guide in https://github.com/PyTables/PyTables/ may help you a lot
 
 # import urllib, httplib2
-import requests, time, hashlib, json, re
+import requests
+import time, hashlib, json, re
 # import pandas as pd
 import numpy as np
 from pandas import Series, DataFrame
 
 from private_data import proxies, agent, login_telephone, login_password
 
-sleeptime = 1    # 休眠时间（单位秒）
+sleeptime = 1          # 休眠时间（单位秒）
 
-Cookie_glo = ''  # 节约资源而来的保存第一次获取的cookie
-Cookie_glo_login = '' # After Login.
+Cookie_glo = ''        # 节约资源而来的保存第一次获取的cookie
+Cookie_glo_login = ''  # After Login.
 
 baseHeader = {
     'Host': 'xueqiu.com',
@@ -72,35 +73,36 @@ def get_md5(password):
 
 def xueqiu_login(telephone, password):
     global Logined
-    url = 'https://xueqiu.com/'
+    # url = 'https://xueqiu.com/'
     headers = GetHeader()
-    headers['Referer'] = "https://xueqiu.com/"
-    login_url_api = "https://xueqiu.com/service/csrf?api=%2Fuser%2Flogin"
+    headers['Referer'] = 'https://xueqiu.com/'
+    login_url_api = 'https://xueqiu.com/service/csrf?api=%2Fuser%2Flogin'
     try:
         request(login_url_api, header=headers)
     except KeyboardInterrupt:
         raise KeyboardInterrupt()
     except:
-        raise Exception("网络错误!")
-    login_url = "https://xueqiu.com/user/login"
+        raise Exception('网络错误！')
+    login_url = 'https://xueqiu.com/user/login'
     postdata = {
-        "areacode": "86",
-        "password": get_md5(password),
-        "remember_me": "on",
-        "telephone": telephone
+        # 'areacode': '86',
+        'areacode': '1',
+        'password': get_md5(password),
+        'remember_me': 'on',
+        'telephone': telephone
     }
     try:
         resp, cont = request(login_url, body=postdata, header=headers, method='POST')
         headers['Cookie'] = resp['Set-Cookie']
-        res_, cont = request("https://xueqiu.com/setting/user", header=headers)
+        res_, cont = request('https://xueqiu.com/setting/user', header=headers)
     except KeyboardInterrupt:
         raise KeyboardInterrupt()
     except:
-        raise Exception("网络错误!")
+        raise Exception('网络错误！')
 
     res = re.findall(r'"profile":"/(.*?)","screen_name":"(.*?)"', cont.decode('utf-8'))
     if res == []:
-        print("登录失败，请检查你的手机号和密码输入是否正确")
+        print('登录失败，请检查你的手机号和密码输入是否正确')
         return ''
     else:
         print('登录成功，你的用户 id 是：%s, 你的用户名是：%s' % (res[0]))
@@ -128,7 +130,7 @@ def GetHeader(login = False):
             except KeyboardInterrupt:
                 raise KeyboardInterrupt()
             except:
-                raise Exception("登陆错误!")
+                raise Exception('登陆错误！')
         Header['Cookie'] = Cookie_glo_login
     else:
         if Cookie_glo == '':
