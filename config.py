53e3ef9d4f37e9212fb212d95a680b6b02ac6801
Oyami-Srk <hhx.xxm@gmail.com>
2017-09-22 01:01:55 +0800

    Configuration File

---
+++ config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#! python3
+
+# You can only modifiy this file when you are really understand what you are doingr
+
+proxies  = {}
+login_telephone = ''
+login_password = ''
+login_areacode = '86'
+agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0'
+monitor_log = ''
+delay_time = 1    #in second
+
